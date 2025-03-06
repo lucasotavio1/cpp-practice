c0b2d03cca41f4c08cf3e44f05de65852dd09c94
lucasotavio <lucasotavioaf@gmail.com>
2025-03-06 20:55:38 -0300

    1008

---
+++ beecrowd/1008/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+   int numero;
+   double horas, valorPorHora;
+
+   cin >> numero >> horas >> valorPorHora;
+
+   double salario = horas * valorPorHora;
+
+   cout << "NUMBER = " << numero << endl;
+   cout << "SALARY = U$ " << fixed << setprecision(2) << salario << endl;
+
+   return 0;
+}
