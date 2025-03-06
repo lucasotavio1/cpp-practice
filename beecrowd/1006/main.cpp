a61f868760e1255e09f212a43b13adffbbc77bf7
lucasotavio <lucasotavioaf@gmail.com>
2025-03-06 20:43:31 -0300

    1006

---
+++ beecrowd/1006/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+   double a, b, c;
+
+   cin >> a >> b >> c;
+
+   double media = (a * 2 + b * 3 + c * 5) / 10;
+
+   cout << "MEDIA = " << fixed << setprecision(1) << media << endl;
+
+   return 0;
+}
