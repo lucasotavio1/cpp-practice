faf404428ef4168a0eb7c99454889a7e81293299
lucasotavio <lucasotavioaf@gmail.com>
2025-03-06 19:59:57 -0300

    1002

---
+++ beecrowd/1002/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+   double raio, area;
+   const double n = 3.14159;
+
+   cin >> raio;
+
+   area = n * raio * raio;
+
+   cout << "A=" << fixed << setprecision(4) << area << endl;
+
+   return 0;
+}
