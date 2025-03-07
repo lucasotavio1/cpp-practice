ba0f1c760c6bfd7e8312ec234b73b94f914543d7
lucasotavio <lucasotavioaf@gmail.com>
2025-03-06 21:27:13 -0300

    1007

--- beecrowd/1007/main.cpp
+++ beecrowd/1007/main.cpp
@@ -3,13 +3,13 @@
 using namespace std;
 
 int main(){
-   int a, b, c, d;
+    int a, b, c, d;
 
-   cin >> a >> b >> c >> d;
+    cin >> a >> b >> c >> d;
 
-   int diferenca = (a * b - c * d);
+    int diferenca = (a * b - c * d);
 
-   cout << "DIFERENCA = " << diferenca << endl;
+    cout << "DIFERENCA = " << diferenca << endl;
 
-   return 0;
+    return 0;
 }
