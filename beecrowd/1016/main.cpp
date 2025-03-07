ecc4e5013e664d251541f7dabcd9063e19d0b255
lucasotavio <lucasotavioaf@gmail.com>
2025-03-07 09:34:49 -0300

    1016

---
+++ beecrowd/1016/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    int distancia;
+
+    cin >> distancia;
+
+    int tempo = distancia * 2;
+
+    cout << tempo << " minutos" << endl;
+
+    return 0;
+}
