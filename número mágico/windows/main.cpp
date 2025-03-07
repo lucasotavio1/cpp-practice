222e424c64417aa510599df6f4aa1d55028ff75f
lucasotavio <lucasotavioaf@gmail.com>
2025-03-07 20:58:20 -0300

    windows

---
+++ número mágico/windows/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <locale.h>
+
+using namespace std;
+
+
+int main() {
+    setlocale(LC_ALL, "Portuguese");
+
+    srand(time(nullptr));
+    int valor, sorteado = rand()%100, tentativas = 0;
+
+    cout << " *** Tente descobrir o número mágico entre 0 e 99. ***\n\n";
+
+    do {
+        cout << "Informe um valor: ";
+        cin >> valor;
+        tentativas++;
+
+        if (valor == sorteado) {
+            cout << "^^ Você acertou com: " << tentativas << " tentativas.\n\n";
+        } else if (valor < sorteado) {
+            cout << "x_x Errou! Digite um número maior.\n\n";
+        } else if (valor > sorteado) {
+            cout << "x_x Errou! Digite um número menor.\n\n";
+        }
+
+    } while (valor != sorteado);
+
+    return 0;
+}
